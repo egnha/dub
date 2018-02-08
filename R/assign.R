6639f9198209fa9daad74a6e0c28c7cbde8f5364
Eugene Ha <eha@posteo.de>
2018-02-08 09:04:05 +0100

    Initial implementation of assignment operators

---
+++ R/assign.R
@@ -0,0 +1,48 @@
+#' Assign names to the components of a list or vector
+#'
+#' @param pattern Assignment pattern (see examples).
+#' @param value List or vector.
+#'
+#' @return Returns `value` invisibly.
+#'
+#' @examples
+#' (one : two) %<=% c(1, 2)
+#' (one : two) %<=% 1:2
+#' (p : (q : r : s)) %<=% list(1, list(2, 3, 4))
+#' (a : (. : (c : d) : e)) %<=% list(1, list(2, list(3, 4), 5))
+#' (. : (. : b : .)) %<=% list(1, list(2, list(3, 4), 5))
+#' (z : .) %<=% list(list(1))
+#' ((x : y)) %<=% list(list(1, 2))
+#'
+#' @export
+#' @name assign
+`%<=%` <- function(pattern, value) {
+  nms <- positioned_names(substitute(pattern))
+  val <- as.list(value)
+  for (path in index_paths(nms)) {
+    nm <- nms[[path]]
+    if (nm != ".")
+      assign(nm, val[[path]], envir = parent.frame())
+  }
+  invisible(value)
+}
+
+#' @export
+#' @rdname assign
+`%=>%` <- opp(`%<=%`)
+
+positioned_names <- function(expr) {
+  nms <- do.call("substitute", list(as_strings(expr), struts))
+  eval(nms)[[1]]  # Strip enclosing list
+}
+
+as_strings <- function(expr) {
+  if (!is.call(expr))
+    return(as.character(expr))
+  for (i in 2:length(expr))
+    expr[[i]] <- Recall(expr[[i]])
+  expr
+}
+
+struts <- list("(" = quote(list), ":" = quote(concat))
+concat <- function(...) as.list(c(...))
---
+++ R/util.R
@@ -0,0 +1,39 @@
+opp <- function(f) {
+  formals(f) <- rev(formals(f))
+  f
+}
+
+#' Find all index paths of (the leaves of) a list
+#'
+#' @param x List.
+#' @return List of index paths (integer vectors).
+#'
+#' @examples
+#' x <- list(1, b = "b", c = list(d = "d", list(4)))
+#' paths <- index_paths(x)
+#' lapply(paths, function(path) x[[path]])
+#' #> [[1]]
+#' #> [1] 1
+#' #>
+#' #> [[2]]
+#' #> [1] "b"
+#' #>
+#' #> [[3]]
+#' #> [1] "d"
+#' #>
+#' #> [[4]]
+#' #> [1] 4
+#'
+#' @noRd
+index_paths <- function(x) {
+  if (!is.list(x))
+    return(NULL)
+  indices <- lapply(seq_along(x), function(i) {
+    if (is.list(x[[i]]) && length(x[[i]])) {
+      sub_paths <- index_paths(x[[i]])
+      lapply(sub_paths, function(path) c(i, path))
+    } else
+      list(i)
+  })
+  do.call("c", indices)
+}
