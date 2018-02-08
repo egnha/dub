98398a61ce272012b0c3a692131419e28f95ebe9
Eugene Ha <eha@posteo.de>
2018-02-08 09:07:18 +0100

    Remove redundant braces

--- R/util.R
+++ R/util.R
@@ -28,12 +28,12 @@ opp <- function(f) {
 index_paths <- function(x) {
   if (!is.list(x))
     return(NULL)
-  indices <- lapply(seq_along(x), function(i) {
+  indices <- lapply(seq_along(x), function(i)
     if (is.list(x[[i]]) && length(x[[i]])) {
       sub_paths <- index_paths(x[[i]])
       lapply(sub_paths, function(path) c(i, path))
     } else
       list(i)
-  })
+  )
   do.call("c", indices)
 }
