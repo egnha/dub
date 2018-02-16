2cca7f85dac205abe2202e11a791142de669c0a1
Eugene Ha <eha@posteo.de>
2018-02-16 11:59:55 +0100

    Refine docs

--- R/assign.R
+++ R/assign.R
@@ -1,21 +1,25 @@
-#' Assign multiple components of a list to names
+#' Assign nested components of a list to names
 #'
-#' The \code{\%<=\%} operator enables you to assign multiple components of a
-#' list (or vector) to names, via _pattern matching_. This is especially
-#' convenient when a function returns multiple values as a list and you want to
-#' assign individual names to them.
+#' \code{\%<=\%} is an operator that enables you to assign nested components of
+#' a list (or vector) to names via _pattern matching_. This is especially
+#' convenient for:
+#' - assigning individual names to the multiple values that a function may
+#'   return as the components of a list
+#' - extracting deeply nested list components
 #'
-#' @param pattern Assignment pattern (see below).
+#' @param pattern Pattern of names that the components of `value` are assigned
+#'   to (see below).
 #' @param value List or vector.
 #'
 #' @return Returns `value` invisibly.
 #'
-#' @section Assignment pattern: An _assignment pattern_ is a concise, ad hoc
-#'   syntax for matching names to the (nested) components of a list, by
-#'   mirroring the structure of the list. Use pairs of parentheses (`()`) to
-#'   indicate a list and a colon (`:`) to indicate successive names. A specific
-#'   component, or range of components, can be skipped with a dot (`.`), or dots
-#'   (`...`).
+#' @section Pattern-matching names: Names are matched to the (nested) components
+#'   of a list using an ad hoc pattern-matching syntax (coming from
+#'   [Haskell](https://en.wikibooks.org/wiki/Haskell/Pattern_matching)) that
+#'   mirrors the structure of the list. Use pairs of parentheses (`()`) to
+#'   indicate a list, and a colon (`:`), rather than a comma, to indicate
+#'   successive names. Use a dot (`.`) to skip assignment of a specific
+#'   component, or dots (`...`) to skip assignment of a range of components.
 #'
 #' @examples
 #' ## assign successive components
