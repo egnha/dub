d666e671a855afb54fd3cfd5c1e5ced4497c1d3c
Eugene Ha <eha@posteo.de>
2018-02-16 14:13:47 +0100

    Finesse doc

--- R/assign.R
+++ R/assign.R
@@ -1,9 +1,8 @@
 #' Assign nested components of a list to names
 #'
-#' \code{\%<=\%} is an operator that enables you to assign multiple, possibly
-#' nested components of a list (or vector) to names via pattern matching
-#' (“unpacking assignment”). Think of ‘`<=`’ as a pictograph representing
-#' multiple ‘`<-`’s.
+#' \code{\%<=\%} is an operator that enables you to assign multiple (nested)
+#' components of a list (or vector) to names via pattern matching (“unpacking
+#' assignment”). Think of ‘`<=`’ as a pictograph representing multiple ‘`<-`’s.
 #' \cr\cr
 #' \code{\%<=\%} is especially convenient for:
 #' - assigning individual names to the multiple values that a function may
@@ -17,13 +16,18 @@
 #' @return Returns `value` invisibly.
 #'
 #' @section Pattern-matching names: Names are matched to the (nested) components
-#'   of a list using an ad hoc pattern-matching syntax (coming from
-#'   [Haskell](https://en.wikibooks.org/wiki/Haskell/Pattern_matching)) that
-#'   mirrors the structure of the list. Use pairs of parentheses (`()`) to
-#'   indicate a list, and a colon (`:`), rather than a comma, to indicate
-#'   successive names. Use a dot (`.`) to skip assignment of a specific
-#'   component, or dots (`...`) to skip assignment of a range of components. The
-#'   examples illustrate some common use cases.
+#'   of a list using a concise pattern-matching syntax (similar to
+#'   [Haskell](https://en.wikibooks.org/wiki/Haskell/Pattern_matching)’s) that
+#'   mirrors the structure of the list. Apart from names, the syntax consists of
+#'   two classes of symbols:
+#'
+#'   - **List constructors** — Use a pair of parentheses (`()`) to indicate a
+#'     list, and a colon (`:`), rather than a comma, to indicate successive names.
+#'
+#'   - **Wildcards** — Use a dot (`.`) to skip assignment of a specific
+#'     component, or dots (`...`) to skip assignment of a range of components.
+#'
+#'   See the examples for an illustration of common use cases.
 #'
 #' @section Prior art: Unpacking/multiple assignment appears in other
 #'   languages, e.g.,
@@ -37,28 +41,28 @@
 #'   [gsubfn](https://cran.r-project.org/package=gsubfn) package.
 #'
 #' @examples
-#' ## assign successive components
+#' # assign successive components
 #' (one : two : three) %<=% list(1, 2, 3)
 #' stopifnot(identical(list(one, two, three), list(1, 2, 3)))
 #'
-#' ## assign nested components
+#' # assign nested components
 #' (p : (q : r : (s : t))) %<=% list(1, list(2, 3, list(4, 5)))
 #' (P : (Q : R : S)) %<=% list(1, list(2, 3, list(4, 5)))
 #' stopifnot(identical(list(p, q, r, s, t), list(1, 2, 3, 4, 5)),
 #'           identical(list(P, Q, R, S), list(1, 2, 3, list(4, 5))))
 #'
-#' ## nested parentheses dig into a list
+#' # nested parentheses unpack nested components
 #' (w) %<=% list(1:3)
 #' (((z))) %<=% list(list(list("z")))
 #' ((x : y)) %<=% list(list("x", "y"))
 #' stopifnot(w == 1:3, identical(list(x, y, z), list("x", "y", "z")))
 #'
-#' ## skip a component with a dot (.)
+#' # skip a component with a dot (.)
 #' (a : . : b) %<=% list("a", "skip this", "b")
 #' ((c : .) : .) %<=% list(list("c", "skip this"), "skip this")
 #' stopifnot(identical(list(a, b, c), list("a", "b", "c")))
 #'
-#' ## skip a range of components with dots (...)
+#' # skip a range of components with dots (...)
 #' (first : ... : last) %<=% letters
 #' (. : second : ...) %<=% letters
 #' (mpg : cyl : ...) %<=% mtcars
